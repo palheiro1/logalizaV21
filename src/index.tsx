aceeeff5cd0dfcb0b2b19daf0b78368df5084e40
palheiro1 <ugioc@riseup.net>
2026-09-14 09:37:28 +0200

    fix: migrate Logaliza to Node 24 and collect Speed Insights

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import "./index.css";
 import "./i18n";
 import App from "./App";
@@ -12,6 +13,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// Use the framework-independent SDK because this app uses React 17.
+injectSpeedInsights();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
