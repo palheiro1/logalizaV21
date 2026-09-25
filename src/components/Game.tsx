4a8496702b03818eea7ad520b9f73de1b463372d
palheiro1 <ugioc@riseup.net>
2026-09-25 09:28:52 +0200

    Atualiza mapa das comarcas para versão detalhada e móvel

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -691,7 +691,7 @@ export function Game({ settingsData, updateSettings, onLoginClick }: GameProps) 
                 <div className="flex justify-center mt-4">
                   <a
                     className="underline text-center mx-8"
-                    href="/maps/mapa-das-comarcas.pdf"
+                    href="/maps/mapa-das-comarcas.html"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
